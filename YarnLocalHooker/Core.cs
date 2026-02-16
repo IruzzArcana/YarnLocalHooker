360a5516e25f5e5ccdb37e4a275feb0322c40d76
IruzzArcana <iruzzgames@gmail.com>
2026-02-16 17:50:43 +0100

    Refractor code

--- YarnLocalHooker/Core.cs
+++ YarnLocalHooker/Core.cs
@@ -18,26 +18,7 @@ namespace YarnLocalHooker
             Config.Init();
 
 #if RELEASE
-            loc = new Il2CppSystem.Collections.Generic.Dictionary<string, string>();
-
-            if (!Il2CppSystem.IO.File.Exists(Config.FilePath))
-            {
-                Melon<Core>.Logger.Error($"{Config.FilePath} not found");
-                return;
-            }
-            var reader = new Il2CppSystem.IO.StreamReader(Config.FilePath);
-            var csv = new CsvReader(reader);
-            csv.Configuration.CultureInfo = CultureInfo.InvariantCulture;
-
-            while (csv.Read())
-            {
-                string key = csv.GetField(0);
-                string value = csv.GetField(1);
-                loc.Add(key, value);
-            }
-
-            csv.Dispose();
-            reader.Dispose();
+            Localization.LoadCSV(Config.FilePath, out loc);
 #endif
         }
     }
@@ -48,44 +29,9 @@ namespace YarnLocalHooker
         public static void Postfix(string __0, ref string __result)
         {
 #if DEBUG
-            Il2CppSystem.IO.StreamReader reader = null;
-            CsvReader csv = null;
-            try
-            {
-                if (!Il2CppSystem.IO.File.Exists(Config.FilePath))
-                {
-                    MelonLogger.Error($"{Config.FilePath} not found");
-                    return;
-                }
-                reader = new(Config.FilePath);
-                csv = new(reader);
-                csv.Configuration.CultureInfo = CultureInfo.InvariantCulture;
-
-                while (csv.Read())
-                {
-                    string key = csv.GetField(0);
-                    string value = csv.GetField(1);
-
-                    if (key == __0)
-                    {
-                        __result = value;
-                        break;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MelonLogger.Error(ex.Message);
-            }
-            finally
-            {
-                if (csv != null)
-                    csv.Dispose();
-                
-                if (reader != null)
-                    reader.Dispose();
-            }
-
+            Localization.GetLocalizedStringFromCSV(Config.FilePath, __0, out string value);
+            if (value != null)
+                __result = value;
 #else
             Core.loc.TryGetValue(__0, out string value);
             if (value != null)
---
+++ YarnLocalHooker/Localization.cs
@@ -0,0 +1,84 @@
+﻿using Il2CppCsvHelper;
+using Il2CppSystem.Globalization;
+using MelonLoader;
+namespace YarnLocalHooker
+{
+    internal class Localization
+    {
+        internal static void LoadCSV(string filepath, out Il2CppSystem.Collections.Generic.Dictionary<string, string> loc)
+        {
+            loc = null;
+            var reader = new Il2CppSystem.IO.StreamReader(filepath);
+            var csv = new CsvReader(reader);
+            try
+            {
+                loc = new();
+
+                if (!Il2CppSystem.IO.File.Exists(filepath))
+                {
+                    Melon<Core>.Logger.Error($"{filepath} not found");
+                    return;
+                }
+                csv.Configuration.CultureInfo = CultureInfo.InvariantCulture;
+
+                while (csv.Read())
+                {
+                    string key = csv.GetField(0);
+                    string value = csv.GetField(1);
+                    loc.Add(key, value);
+                }
+            }
+            catch (Exception ex)
+            {
+                MelonLogger.Error(ex.Message);
+            }
+            finally
+            {
+                csv.Dispose();
+                reader.Dispose();
+            }
+        }
+
+        internal static void GetLocalizedStringFromCSV(string filepath, string key, out string value)
+        {
+            Il2CppSystem.IO.StreamReader reader = null;
+            CsvReader csv = null;
+            value = null;
+            try
+            {
+                if (!Il2CppSystem.IO.File.Exists(filepath))
+                {
+                    MelonLogger.Error($"{filepath} not found");
+                    return;
+                }
+                reader = new(filepath);
+                csv = new(reader);
+                csv.Configuration.CultureInfo = CultureInfo.InvariantCulture;
+
+                while (csv.Read())
+                {
+                    string _key = csv.GetField(0);
+                    string _value = csv.GetField(1);
+
+                    if (_key == key)
+                    {
+                        value = _value;
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MelonLogger.Error(ex.Message);
+            }
+            finally
+            {
+                if (csv != null)
+                    csv.Dispose();
+
+                if (reader != null)
+                    reader.Dispose();
+            }
+        }
+    }
+}
